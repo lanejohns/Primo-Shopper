033e8042c1a4db102f7347b66776502bc59f13bb
Lane <lanedaltonjohns@gmail.com>
2021-11-11 16:42:18 -0500

    created Product component

--- src/products.js
+++ src/products.js
@@ -3,7 +3,7 @@ const products = [
   {
     '_id': '1',
     'name': 'Airpods Wireless Bluetooth Headphones',
-    'image': '/images/airpods.jpg',
+    'image': '/images/airpods.jpeg',
     'description':
       'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
     'brand': 'Apple',
@@ -16,7 +16,7 @@ const products = [
   {
     '_id': '2',
     'name': 'iPhone 11 Pro 256GB Memory',
-    'image': '/images/phone.jpg',
+    'image': '/images/phone.jpeg',
     'description':
       'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
     'brand': 'Apple',
@@ -29,7 +29,7 @@ const products = [
   {
     '_id': '3',
     'name': 'Cannon EOS 80D DSLR Camera',
-    'image': '/images/camera.jpg',
+    'image': '/images/camera.jpeg',
     'description':
       'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
     'brand': 'Cannon',
@@ -42,7 +42,7 @@ const products = [
   {
     '_id': '4',
     'name': 'Sony Playstation 4 Pro White Version',
-    'image': '/images/playstation.jpg',
+    'image': '/images/playstation.jpeg',
     'description':
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     'brand': 'Sony',
@@ -55,7 +55,7 @@ const products = [
   {
     '_id': '5',
     'name': 'Logitech G-Series Gaming Mouse',
-    'image': '/images/mouse.jpg',
+    'image': '/images/mouse.jpeg',
     'description':
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
     'brand': 'Logitech',
@@ -68,7 +68,7 @@ const products = [
   {
     '_id': '6',
     'name': 'Amazon Echo Dot 3rd Generation',
-    'image': '/images/alexa.jpg',
+    'image': '/images/alexa.jpeg',
     'description':
       'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
     'brand': 'Amazon',
