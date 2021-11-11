f299ff6348aa743e20860ebfed684d0c0b60d5c6
Lane <lanedaltonjohns@gmail.com>
2021-11-11 16:28:00 -0500

    added HomeScreen componenet to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
 import { Container } from 'react-bootstrap'
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
