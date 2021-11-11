d3b1330acd2f257d3229ed996f35fbe16a2e1b58
Lane <lanedaltonjohns@gmail.com>
2021-11-11 15:15:10 -0500

    add icons for header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Header from './components/Header'
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap'
 
 function App() {
   return (
     <div>
       <Header />
-      <main>
-        <h1>Welcome</h1>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
       </main>
       <Footer />
     </div>
