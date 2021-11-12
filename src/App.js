c1f59f4288d857be0d879a3c82fc4dc9670c95cb
Lane <lanedaltonjohns@gmail.com>
2021-11-12 12:33:13 -0500

    started product screen component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header'
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -13,6 +14,7 @@ function App() {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
