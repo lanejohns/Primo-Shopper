32ba5abb842ec8510603d664a9c6d1d686378153
Lane <lanedaltonjohns@gmail.com>
2021-11-16 15:41:04 -0500

    add disabled ternary

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -53,7 +53,7 @@ function ProductScreen({ match }) {
                                 </Row>
                             </ListGroup.Item>
                             <ListGroup.Item>
-                                <Button variant="primary" type="button">Add To Cart</Button>
+                                <Button variant="primary" type="button" disabled={product.countInStock == 0}>Add To Cart</Button>
                             </ListGroup.Item>
                         </ListGroup>
                     </Card>
