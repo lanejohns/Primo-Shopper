8a6a47a25f66db28194dcdecacc950a2c65f1710
Lane <lanedaltonjohns@gmail.com>
2021-12-03 13:48:08 -0500

    starting Product model/class

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Product(models.Model):
+    user = 
+    name = 
+    #image = 
+    brand = 
+    category = 
+    description = 
+    rating = 
+    numReviews = 
+    price = 
+    countInStock = 
+    createdAt = 
+    _id = 
