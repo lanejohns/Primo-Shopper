056a38410b9493945113d33c16ed9f175798122d
Lane <lanedaltonjohns@gmail.com>
2021-12-03 13:54:47 -0500

    finish Product model/class

--- backend/base/models.py
+++ backend/base/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
-    user = 
-    name = 
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
     #image = 
-    brand = 
-    category = 
-    description = 
-    rating = 
-    numReviews = 
-    price = 
-    countInStock = 
-    createdAt = 
-    _id = 
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
