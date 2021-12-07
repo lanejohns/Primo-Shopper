f8a566a22f79e5c9f76fba0a983ceffd590c16e2
Lane <lanedaltonjohns@gmail.com>
2021-12-07 11:46:58 -0500

    replace products state with products store on HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,23 +7,29 @@ import Product from "../components/Product"
 
 function HomeScreen() {
     const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error, loading, products} = productList
 
     useEffect(() => {
         dispatch(listProducts())
         
-    }, [])
+    }, [dispatch])
 
-    const products = []
     return (
         <div>
             <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                       <Product product={product}/>
-                    </Col>
-                ))}
-            </Row>
+            {loading ? <h2>Loading...</h2>
+                : error ? <h3>{error}</h3>
+                    :
+                    <Row>
+                    {products.map(product => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product}/>
+                        </Col>
+                    ))}
+                    </Row>
+        }
+
         </div>
     )
 }
