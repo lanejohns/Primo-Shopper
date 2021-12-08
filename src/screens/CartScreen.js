524faaa8cb3cd049ad560cbb35398464d0794245
Lane <lanedaltonjohns@gmail.com>
2021-12-08 12:23:34 -0500

    structure for cart and change qty functionality

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -28,10 +28,35 @@ function CartScreen({match, location, history}) {
                 <h1>Shopping Cart</h1>
                 {cartItems.length === 0 ? (
                     <Message variant='info'>
-                        Your cart is empty <Link t0='/'>Go Back</Link>
+                        Your cart is empty <Link to='/'>Go Back</Link>
                     </Message>
                 ) : (
-                    <ListGroup variant='flush'></ListGroup>
+                    <ListGroup variant='flush'>
+                        {cartItems.map(item => (
+                            <ListGroup.Item key={item.product}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded/>
+                                    </Col>
+                                    <Col md={3}>
+                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                    </Col>
+                                    <Col md={2}>
+                                        ${item.price}
+                                    </Col>
+                                    <Col md={3}>
+                                        <Form.Select as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product, e.target.value))}>
+                                                {
+                                                    [...Array(item.countInStock).keys()].map((x) => (
+                                                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                                    ))
+                                                }
+                                            </Form.Select>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
                 )}
             </Col>
 
