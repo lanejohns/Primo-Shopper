c3af4c2be841e838dc5fb46112e13abbd7de5d7e
Lane <lanedaltonjohns@gmail.com>
2021-12-08 15:08:15 -0500

    Remove alt setting key from simple jwt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,7 +62,6 @@ SIMPLE_JWT = {
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
-    'SIGNING_KEY': settings.SECRET_KEY,
     'VERIFYING_KEY': None,
     'AUDIENCE': None,
     'ISSUER': None,
