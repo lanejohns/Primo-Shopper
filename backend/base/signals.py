9a1897c78582019db66b330e1c09daf65f60e0a7
Lane <lanedaltonjohns@gmail.com>
2021-12-09 14:41:05 -0500

    Complete signal to change username to email

--- backend/base/signals.py
+++ backend/base/signals.py
@@ -2,6 +2,9 @@ from django.db.models.signals import pre_save
 from django.contrib.auth.models import User
 
 def updateUser(sender, instance, **kwargs):
-    print('Signal Triggered')
+    # this allows us to use the email as a username rather than having to change the Django User model itself
+    user = instance
+    if user.email != '':
+        user.username = user.email
 
 pre_save.connect(updateUser, sender=User)
