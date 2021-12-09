baf7e34f59c656ec30fbe2b097f19eb03cb5dfa3
Lane <lanedaltonjohns@gmail.com>
2021-12-09 14:24:17 -0500

    Add try/except block for error handling

--- backend/base/views.py
+++ backend/base/views.py
@@ -15,6 +15,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 # Create your views here.
 
@@ -35,14 +36,19 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(['POST'])
 def registerUser(request):
     data = request.data
-    user = User.objects.create(
-        first_name=data['name'],
-        username=data['email'],
-        email=data['email'],
-        password=make_password(data['password'])
-    )
-    serializer = UserSerializerWithToken(user, many=False)
-    return Response(serializer.data)
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
