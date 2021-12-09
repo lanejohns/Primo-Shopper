9b44fefd3594eb769edac0e705f1d21c9095f479
Lane <lanedaltonjohns@gmail.com>
2021-12-09 11:27:12 -0500

    Add additional fields to user serializer

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -4,9 +4,28 @@ from .models import Product
 
 
 class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = User
-        fields = '__all__'
+        # Django naturally sets id without an underscore, our other models have _id as a chosen convetion.
+        # Including '_id' in the fields list below as the method get__id allows us to access both
+        # the django id and our chosen _id
+        fields = ['id', 'username', 'email', 'name', '_id', 'isAdmin']
+
+    def get__id(self, obj):
+        return obj.id
+
+    def get_isAdmin(self, obj):
+        return obj.is_staff
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name =='':
+            name = obj.email
+
+        return name
 
 
 class ProductSerializer(serializers.ModelSerializer):
