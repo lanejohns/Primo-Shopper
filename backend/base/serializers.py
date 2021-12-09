8adba0830bcc3a79347a0a53240a06bff377a453
Lane <lanedaltonjohns@gmail.com>
2021-12-09 11:32:59 -0500

    UserSerializerWithToken

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product
 
 
@@ -12,7 +13,7 @@ class UserSerializer(serializers.ModelSerializer):
         # Django naturally sets id without an underscore, our other models have _id as a chosen convetion.
         # Including '_id' in the fields list below as the method get__id allows us to access both
         # the django id and our chosen _id
-        fields = ['id', 'username', 'email', 'name', '_id', 'isAdmin']
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin']
 
     def get__id(self, obj):
         return obj.id
@@ -26,6 +27,18 @@ class UserSerializer(serializers.ModelSerializer):
             name = obj.email
 
         return name
+
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields = ['id', '_id', 'username', 'email', 'name', 'isAdmin', 'token']
+    
+    def get_token(self, obj):
+        # As we generate/serialize a user, we take the user obj and return another token with the initial response
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
 
 
 class ProductSerializer(serializers.ModelSerializer):
