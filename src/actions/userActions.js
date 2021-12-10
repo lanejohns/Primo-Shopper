8935d58bdf60a132d5a900447ee6b95941efc2f8
Lane <lanedaltonjohns@gmail.com>
2021-12-10 15:58:23 -0500

    set up login functionality

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -15,13 +15,13 @@ export const login = (email, password) => async (dispatch) => {
 
         const config = {
             headers: {
-                'Content-type': 'application',
+                'Content-type': 'application/json',
             }
         }
 
         const {data} = await axios.post(
-            '/api/users/login',
-            { 'username':email, 'password':password },
+            '/api/users/login/',
+            { 'username': email, 'password': password },
             config
             )
         
